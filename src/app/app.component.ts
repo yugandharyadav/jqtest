c553fb0776e8f038252a3bd7aeca17d9befb07a7
aswath <aswath.satrasala@gmail.com>
2018-09-19 16:46:43 +0530

    Most simplified.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,81 +1,39 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-  customReportInfo: any;
-  customReportSearchCriteria: any;
-  gridDataSource: any;
-  gridColumns: any;
-  gridWidth: number;
+  gridColumns: any[];
+  gridWidth  = 200;
+  source = this.createGridDataSource();
+  gridDataSource = new jqx.dataAdapter(this.source);
 
-  constructor(private http: HttpClient) {
-    this.getCustomReportInfo().subscribe(data => {
-      this.customReportInfo = data;
-        console.log(data);
-        this.getCustomReportSearchCriteria().subscribe(data1 => {
-          this.customReportSearchCriteria = data1;
-          console.log(data1);
-          this.gridDataSource = this.createGridDataSource(this.customReportInfo, this.customReportSearchCriteria);
-      });
-   });
-  }
-
-  public getCustomReportInfo(): Observable<any> {
-      return this.http.get('../sampledata/customreportinfo.json');
-  }
-
-  public getCustomReportSearchCriteria(): Observable<any> {
-    return this.http.get('../sampledata/customreportsearchcriteria.json');
-  }
-
-  createGridDataSource(customReportInfo: any, customReportSearchCriteria: any): any {
-    this.gridColumns = customReportInfo.jqGridColumnList;
+  createGridDataSource(): any {
     const data = new Array();
-    this.gridWidth = 900;
-    let columnCount = 0;
 
-    const customReportOutPutMetaDataList = customReportSearchCriteria.customReportOutPutMetaDataList;
+    this.gridColumns = [
+      {datafield: 'No', text: 'No'}
+    ];
 
-    const customReportResults: any = new Array(customReportSearchCriteria.customResultList.length - 1);
-    let k = 0;
-    for (let i = 1; i < customReportSearchCriteria.customResultList.length; i++) {
-      customReportResults[k++] = customReportSearchCriteria.customResultList[i];
-    }
-
-    for (let i = 0; i < customReportResults.length; i++) {
-      const row = {};
-      const rowData = customReportResults[i];
-      for (let j = 0; j < customReportOutPutMetaDataList.length; j++) {
-        const outputMetaData = customReportOutPutMetaDataList[j];
-        if (outputMetaData.dataType && outputMetaData.dataType === 'number') {
-          row[outputMetaData.displayName] = parseFloat(rowData[j]);
-        } else {
-          row[outputMetaData.displayName] = rowData[j];
-        }
-      }
-      data.push(row);
-    }
-
-    const dataFieldArr = new Array();
-
-    for (const outputMetaData of customReportOutPutMetaDataList) {
-      const dataField = {
-        name: outputMetaData.displayName,
-        type: outputMetaData.dataType
+    for (let i = 0; i < 3; i++) {
+      const row = {
       };
-      dataFieldArr.push(dataField);
+      row['No'] = 'Inv1';
+      data[i] = row;
     }
 
     // create a data source and data adapter
     const source = {
       localdata: data,
       datatype: 'array',
-      datafields: dataFieldArr
+
+      datafields:
+      [
+          { name: 'No', type: 'string' }
+      ]
+
     };
     return source;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { jqxGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid';
-import { jqxButtonComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxbuttons';
 
 @NgModule({
   declarations: [
     AppComponent,
-    jqxGridComponent,
-    jqxButtonComponent
+    jqxGridComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
