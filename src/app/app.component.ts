fde554c217f344ef563da69a40be2a485dd79437
yugandhar <yugandhar@veersoftsolutions.com>
2018-10-05 11:15:17 +0530

    Introduced prev and next button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   gridColumns: any[];
-  gridWidth  = 200;
-  source = this.createGridDataSource();
+  gridWidth = 200;
+  source = this.createGridDataSource(false);
   gridDataSource = new jqx.dataAdapter(this.source);
+  showNext = true;
+  showPrev = false;
 
-  createGridDataSource(): any {
+  createGridDataSource(next): any {
     const data = new Array();
 
     this.gridColumns = [
-      {datafield: 'No', text: 'No'}
+      { datafield: 'No', text: 'No' },
+      { datafield: 'Quantity', text: 'Quantity', aggregates: ['count'] }
     ];
 
-    for (let i = 0; i < 3; i++) {
-      const row = {
-      };
-      row['No'] = 'Inv1';
-      data[i] = row;
+    if (next) {
+      for (let i = 0; i < 3; i++) {
+        const row = {
+        };
+        row['No'] = 'Inv2';
+        row['Quantity'] = i;
+        data[i] = row;
+      }
+    } else {
+      for (let i = 0; i < 3; i++) {
+        const row = {
+        };
+        row['No'] = 'Inv1';
+        row['Quantity'] = i;
+        data[i] = row;
+      }
     }
 
     // create a data source and data adapter
@@ -30,11 +44,24 @@ export class AppComponent {
       datatype: 'array',
 
       datafields:
-      [
-          { name: 'No', type: 'string' }
-      ]
+        [
+          { name: 'No', type: 'string' },
+          { name: 'Quantity', type: 'number' },
+        ]
 
     };
     return source;
+  }
+
+  next() {
+    this.showNext = false;
+    this.showPrev = true;
+    this.gridDataSource = this.createGridDataSource(true);
+  }
+
+  prev() {
+    this.showNext = true;
+    this.showPrev = false;
+    this.gridDataSource = this.createGridDataSource(false);
   }
 }
